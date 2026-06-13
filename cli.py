37d12a45e1eac0d851590c469715b6143825c7ed
Lucy <lucymwago9@gmail.com>
2026-06-13 12:13:06 +0300

    add project

--- cli.py
+++ cli.py
@@ -5,8 +5,15 @@ parser = argparse.ArgumentParser(
 )
 
 subparsers = parser.add_subparsers(dest = "command")
-
+#add-user
 add_user = subparsers.add_parser("add-user")
 add_user.add_argument("username")
 
+#list-users
 list_users = subparsers.add_parser("list-users")
+
+#add-project
+add_project = subparsers.add_parser("add-project")
+
+add_project.add_argument("username")
+add_project.add_argument("project_name")
